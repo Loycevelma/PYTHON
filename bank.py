206d51328e0b74030b1476de05d381ca85e4846c
Loycevelma <79199328+Loycevelma@users.noreply.github.com>
2021-06-09 13:23:54 +0300

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 # class Bank:
 #     def __init__(self,name,age,ID):
 #         self.name=name
@@ -19,11 +20,14 @@ class Account:
         self.balance=0
         self.loan=0
         self.loan_limit=20000
+        self.transaction=[]
+        self.loan=20000
 
     def deposit(self,amount):
         self.balance+=amount
-        return f"Dear {self.name} you have deposited {amount} andyour balance is{self.balance}"   
-
+        transaction={"amount":amount,"balance":self.balance,"time":datetime.now(),"narration":"Deposit"}
+        self.transaction.append(transaction)
+        return self.show_balance()  
     def show_balance(self):
 
         return f"Dear {self.name} you have deposited {self.amount} ypour new balance  is {self.balance}"    
@@ -37,13 +41,19 @@ class Account:
          return f"You have deposited{amount} and your balance is{self.balance}"  
 
     def withdraw(self,amount):
+
         if  amount<0:
             return f"The amount is lesser than zero"
         elif amount>self.balance:
             return f"Transaction not succesful"
         else:
             self.balance -= amount
-            return f"Dear {self.name}, you have withdraw{amount} and your balance is{self.balance}"
+          
+
+        withdraw={"amount":amount,"balance":self.balance,"time":datetime.now(),"narration":"Deposit"}
+        self.transaction.append(withdraw)
+        return f"Dear {self.name}, you have withdraw{amount} and your balance is{self.balance}"
+        
 
     def borrow(self,amount):  
         if amount<=self.loan_limit:
@@ -54,8 +64,60 @@ class Account:
 
 
         else:
-           return  f"Dear {self.name},your loan of {self.loan}  is  not approved because the amount you want to borrow is above your limit{self.balance}"
+         return  f"Dear {self.name},your loan of {self.loan}  is  not approved because the amount you want to borrow is above your limit{self.balance}"
+
+       
+    def get_statement(self):
+        for transaction in self.transaction:
+            narration=transaction["narration"]
+            amount=transaction["amount"]
+            balance=transaction["balance"]
+            time=transaction["time"]
+            print("{}:You have made a {} of {}.Your new balance is {}.".format(time,narration,amount,balance)) 
+
+    def withdraw(self,amount):
+      if amount<0:
+       return f"Dear {self.name} You can't borrow  amount"
+      elif self.balance<amount:
+       return f"Dear {self.name} Your balance is low."
+      else:
+       self.balance-=amount      
+       return f"Dear {self.name}, You have withdrawn {amount}, Your balance is {self.balance}"
+    def  show_balance(self):
+     return self.balance
+    def borrow(self,amount):
+      if amount >=self. loan_limit:  
+       return f"Dear{self. name},the amount you want to withdraw is aroud to withdraw"
+      elif self.loan>0:
+       return f"Dear{self.name}your loan have not been paid" 
+      else:
+          self.loan+=1
+          self.balance+=amount
+      return f"Dear {self.name}, You have borrowed {amount} your new balance is{self.balance}" 
 
 
-    
+
+    def repay(self,amount):
+         if amount > 0:
+                 return f"Dear {self.name} your loan is not valid"
+         elif amount>self.loan:
+            self.loan-=amount
+            return f"Dear {self.name} your loan has been decreased {self.loan}"
+         else:
+            diff=amount-self.loan
+            self.laon=0
+            self.deposit(diff)
+            self.balance+=amount
+            now=datetime.now()
+            transaction={
+               "amount":amount,
+               "time":now,
+               "narration":"you have paid your loan"
+               
+            } 
+            self.statement.append(transaction)
+            return f"Dear {self.name} your loan has been cleared"          
+            
+
+
 
